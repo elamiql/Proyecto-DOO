80b80e134df63c8c66955ab3990d3318bb4de395
estrikee <jpnunez.toloza@gmail.com>
2025-07-03 15:35:07 -0400

    Tests resultados y excepcion test torneo equipo
    se añadió la excepción de numero de equipos invalido al test de torneo equipo

--- src/test/java/org/example/model/TorneoEquipoTest.java
+++ src/test/java/org/example/model/TorneoEquipoTest.java
@@ -2,6 +2,7 @@ package org.example.model;
 
 import org.example.enums.Formato;
 import org.example.exceptions.FormatoInvalidoException;
+import org.example.exceptions.ParticipantesInsuficientesException;
 import org.example.interfaces.Disciplina;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,7 +29,14 @@ class TorneoEquipoTest {
     void generarCalendario_formatoInvalido() {
         TorneoEquipo Torneo_error = new TorneoEquipo("Torneo Error", disciplina,"25-12-2025 14:30", Formato.CUSTOM, "Online");
 
-        assertThrows(FormatoInvalidoException.class, Torneo_error::generarCalendario);
+        assertThrows(NumberFormatException.class, Torneo_error::generarCalendario);
+    }
+
+    @Test
+    void gruposMenores0() {
+        TorneoEquipo Torneo_error = new TorneoEquipo("Torneo Error", disciplina,"25-12-2025 14:30", Formato.GRUPOS_CON_ELIMINATORIA,0,0,"online" );
+
+        assertThrows(ParticipantesInsuficientesException.class, Torneo_error::generarCalendario);
     }
 
     @AfterEach
